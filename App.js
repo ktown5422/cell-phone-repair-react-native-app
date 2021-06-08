d596609b73401594d1c7912de18877fc4b782457
Kevin Townson <townsonk@c02crn5pmd6m.wwt.com>
2021-06-08 10:59:53 -0500

    adding stack, tab and drawer navigation with login screen

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({ navigation, route }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to ProfileScreen"
+        onPress={() => {
+          navigation.push('ProfileScreen')
+        }}
+      />
+    </View>
+    
+  );
+}
+
+function ProfileScreen({ navigation, route }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button
+        title="Go back"
+        onPress={() => {
+          navigation.pop()
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function InventoryScreen({ navigation, route }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Inventory Screen</Text>
+    </View>
+  );
+}
+
+function SetAppointmentScreen({ navigation, route }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Set Appointment Screen</Text>
+    </View>
+  );
+}
+
+const AppStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="HomeScreen" headerMode="none">
+      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Dashboard' }} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="InventoryScreen" component={InventoryScreen} />
+      <Stack.Screen name="SetAppointmentScreen" component={SetAppointmentScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const AppTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Dashboard' }} />
+      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({});
+
+
+export default LoginScreen;
